8b2313aee1ccc5558edea95039b3a2be4ede58b7
shailifadadu <116940934+shailifadadu@users.noreply.github.com>
2025-01-15 18:02:15 +0530

    Fixed error for deployment

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -55,6 +55,9 @@ export const useChatStore = create((set, get) => ({
     const socket = useAuthStore.getState().socket;
 
     socket.on("newMessage", (newMessage) => {
+      const isMessageSentFromSelectedUser =
+        newMessage.senderId === selectedUser._id;
+      if (isMessageSentFromSelectedUser) return;
       set({
         messages: [...get().messages, newMessage],
       });
